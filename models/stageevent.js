552477fddee89e8cccb1759279240767bcecf8c2
Nick Lafauci <nicholas.lafauci@gmail.com>
2023-09-11 19:01:51 -0400

    music tour associations upgrade

--- models/stageevent.js
+++ models/stageevent.js
@@ -1,7 +1,7 @@
-'use strict';
+'use strict'
 const {
   Model
-} = require('sequelize');
+} = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class StageEvent extends Model {
     /**
@@ -12,13 +12,26 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  }
+  };
   StageEvent.init({
-    stageevent_id: DataTypes.INTEGER,
-    stageevent_name: DataTypes.STRING
+    stage_events_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    stage_id: {
+      type: DataTypes.SMALLINT,
+      allowNull: false
+    },
+    event_id: {
+      type: DataTypes.SMALLINT,
+      allowNull: false
+    }
   }, {
     sequelize,
     modelName: 'StageEvent',
-  });
-  return StageEvent;
-};
+    tableName: 'stage_events',
+    timestamps: false,
+  })
+  return StageEvent
+}
